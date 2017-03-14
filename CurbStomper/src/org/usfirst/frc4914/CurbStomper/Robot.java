8ccf23abd2b309cb1dac587ef10e6546808c646b
cyamonide <cyamonide@gmail.com>
2017-03-14 11:40:56 -0400

    Greyscale images to lower bandwidth
    In CurbStomper's robot.java
    Resolution increased to 320x240

--- CurbStomper/src/org/usfirst/frc4914/CurbStomper/Robot.java
+++ CurbStomper/src/org/usfirst/frc4914/CurbStomper/Robot.java
@@ -24,6 +24,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 import org.usfirst.frc4914.CurbStomper.commands.*;
 import org.usfirst.frc4914.CurbStomper.subsystems.*;
 
@@ -79,44 +80,9 @@ public class Robot extends IterativeRobot {
         // initializes networktable
         table = NetworkTable.getTable("HookContoursReport");
         
-        // camera switching code
+        // sets up camera switcher
         server = CameraServer.getInstance();
-        Thread t = new Thread(() -> {
-    		
-    		boolean allowCam1 = false;
-    		
-    		UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture(0);
-            camera1.setResolution(160, 120);
-            camera1.setFPS(30);
-            UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
-            camera2.setResolution(160, 120);
-            camera2.setFPS(30);
-            
-            CvSink cvSink1 = CameraServer.getInstance().getVideo(camera1);
-            CvSink cvSink2 = CameraServer.getInstance().getVideo(camera2);
-            CvSource outputStream = CameraServer.getInstance().putVideo("Switcher", 160, 120);
-            
-            Mat image = new Mat();
-            
-            while(!Thread.interrupted()) {
-            	
-            	if (Robot.oi.getPrimaryJoystick().getRawButton(4)) { allowCam1 = !allowCam1; }
-            	
-                if(allowCam1){
-                  cvSink2.setEnabled(false);
-                  cvSink1.setEnabled(true);
-                  cvSink1.grabFrame(image);
-                } else{
-                  cvSink1.setEnabled(false);
-                  cvSink2.setEnabled(true);
-                  cvSink2.grabFrame(image);     
-                }
-                
-                outputStream.putFrame(image);
-            }
-            
-        });
-        t.start();
+        cameraInit();
         
         // set up sendable chooser for autonomous
         autoChooser = new SendableChooser();
@@ -265,5 +231,52 @@ public class Robot extends IterativeRobot {
      * Changes camera feed
      */
     public static void switchCamera() {
+    }
+    
+    /**
+     * Camera switcher initialization
+     */
+    private void cameraInit() {
+
+        // camera switching code
+        Thread t = new Thread(() -> {
+    		
+    		boolean allowCam1 = false;
+    		
+    		UsbCamera camera1 = CameraServer.getInstance().startAutomaticCapture(0);
+            camera1.setResolution(320, 240);
+            camera1.setFPS(30);
+            UsbCamera camera2 = CameraServer.getInstance().startAutomaticCapture(1);
+            camera2.setResolution(320, 240);
+            camera2.setFPS(30);
+            
+            CvSink cvSink1 = CameraServer.getInstance().getVideo(camera1);
+            CvSink cvSink2 = CameraServer.getInstance().getVideo(camera2);
+            CvSource outputStream = CameraServer.getInstance().putVideo("Switcher", 320, 240);
+            
+            Mat image = new Mat();            
+            Mat grey = new Mat();
+            
+            while(!Thread.interrupted()) {
+            	
+            	if (Robot.oi.getPrimaryJoystick().getRawButton(4)) { allowCam1 = !allowCam1; }
+            	
+                if(allowCam1){
+                  cvSink2.setEnabled(false);
+                  cvSink1.setEnabled(true);
+                  cvSink1.grabFrame(image);
+                } else{
+                  cvSink1.setEnabled(false);
+                  cvSink2.setEnabled(true);
+                  cvSink2.grabFrame(image);     
+                }
+                
+                Imgproc.cvtColor(image, grey, Imgproc.COLOR_BGR2GRAY);
+                
+                outputStream.putFrame(grey);
+            }
+            
+        });
+        t.start();
     }
 }
