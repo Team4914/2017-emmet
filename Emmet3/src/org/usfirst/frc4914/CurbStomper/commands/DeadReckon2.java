64bd120e02458dea036c3245c8fbe0db2d822fc0
cyamonide <cyamonide@gmail.com>
2017-10-29 20:13:16 -0400

    Document DeadReckon2.java

--- Emmet3/src/org/usfirst/frc4914/CurbStomper/commands/DeadReckon2.java
+++ Emmet3/src/org/usfirst/frc4914/CurbStomper/commands/DeadReckon2.java
@@ -6,43 +6,49 @@ import org.usfirst.frc4914.CurbStomper.RobotConstants;
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- *
+ * This command drives the robot in a straight line given a desired distance.
+ * Adjustments to left and right sides are made based on deviations from the initial gyrometer reading.
  */
 public class DeadReckon2 extends Command {
 	
-	double distance = 0;
-	double bearing = 0;
+	double distance = 0; // distance to drive, in inches
+	double speed = 0; // drive speed, in inches per second
 
-    public DeadReckon2(double encoderSetpoint) {
-    	setTimeout(Math.abs(encoderSetpoint) + 1);
-    	distance = encoderSetpoint;
+    public DeadReckon2(double distance) {
+    	this.distance = distance;
+    	this.speed = RobotConstants.AUTO_SPEED;
+    	// calculates timeout in seconds given distance and speed
+    	double timeout = Math.abs(distance/speed);
+    	setTimeout(timeout);
     }
 
     protected void initialize() {
     	// resets sensors
     	Robot.drivetrain.resetGyro();
-    	Robot.drivetrain.resetEncoder();
     	
     	// inverts speed if distance negative
     	if (distance < 0) RobotConstants.AUTO_SPEED *= -1;
     }
 
     protected void execute() {
-    	// variable updates
-    	bearing = Robot.drivetrain.getRawGyroBearing();
+    	// samples gyro
+    	double bearing = Robot.drivetrain.getRawGyroBearing();
     	
-		Robot.drivetrain.tankDrive((RobotConstants.AUTO_SPEED + bearing*RobotConstants.AUTO_DRIVESTRAIGHT_P)*2, 
-				(RobotConstants.AUTO_SPEED - bearing*RobotConstants.AUTO_DRIVESTRAIGHT_P)*2, false, false);
-    	System.out.println(Robot.drivetrain.getRawGyroBearing());
+    	// Drive robot in a straight line by adding/subtracting gyrometer offsets
+		Robot.drivetrain.tankDrive((RobotConstants.AUTO_SPEED + bearing*RobotConstants.AUTO_DRIVESTRAIGHT_P), 
+				(RobotConstants.AUTO_SPEED - bearing*RobotConstants.AUTO_DRIVESTRAIGHT_P), false, false);
+		
+		// Logs raw gyrometer readings as console output
+    	// System.out.println(Robot.drivetrain.getRawGyroBearing());
     }
 
     protected boolean isFinished() {
-        return Math.abs(Robot.drivetrain.getEncoderPosition()) >= Math.abs(distance) || isTimedOut();
+    	// ends command if timed out
+        return isTimedOut();
     }
 
     protected void end() {
     	Robot.drivetrain.stop();
-    	if (distance < 0) RobotConstants.AUTO_SPEED *= -1;
     }
 
     protected void interrupted() {
